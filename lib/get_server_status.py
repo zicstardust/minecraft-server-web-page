3eaf8b6d639e19bb1428e1aa2e0412b9b3bf8692
AimbotParce <marcparcerisa@gmail.com>
2023-05-30 22:57:57 +0200

    Finished first working prototype

--- lib/get_server_status.py
+++ lib/get_server_status.py
@@ -1,2 +1,28 @@
+from mcstatus import JavaServer
+
+server = JavaServer.lookup("localhost:25565")
+
+
 def get_server_status():
-    return {"players": 0, "players_color": "red", "status": "offline", "status_color": "red"}
+    players = 0
+    latency = 9999
+    try:
+        status = server.status()
+        online = True
+        players = status.players.online
+        latency = status.latency
+    except:
+        online = False
+
+    players_color = "green" if players > 0 else "red"
+    latency_color = "green" if latency < 100 else "red"
+    online_color = "green" if online else "red"
+    online_text = "online" if online else "offline"
+
+    server_status = [
+        ["status", online_text, online_color],
+        ["players", players, players_color],
+        ["latency", latency, latency_color],
+    ]
+
+    return server_status
--- simple_server.py
+++ simple_server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     server_status = get_server_status()
-    return render_template("index.html", **server_status)
+    return render_template("index.html", server_status=server_status)
 
 
 app.run(debug=True)
