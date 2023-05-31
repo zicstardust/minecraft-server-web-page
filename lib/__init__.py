dfbac406b9527ebe71ff48b4624a36b9550ace17
AimbotParce <marcparcerisa@gmail.com>
2023-05-31 22:57:15 +0200

    Moved names

--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from .get_server_status import get_server_status
-
-
-__all__ = ["get_server_status"]
--- simple_server.py
+++ simple_server.py
@@ -1,6 +1,6 @@
 # Import flask
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from lib import get_server_status
+from lib.server_status import get_server_status
 
 
 # Configure for port 80
@@ -11,6 +11,12 @@ app = Flask(__name__)
 def index():
     server_status = get_server_status()
     return render_template("index.html", server_status=server_status)
+
+
+# @app.route("/chat")
+# def chat():
+#     chat = get_server_chat()
+#     return render_template("chat.html", chat=chat)
 
 
 app.run(host="0.0.0.0", port=80)
