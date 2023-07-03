aea1d2961ed50ecac052c9b0b4151c7e2597aca9
AimbotParce <marcparcerisa@gmail.com>
2023-07-03 23:00:16 +0200

    Checkpoints with proper style

--- lib/checkpoints.py
+++ lib/checkpoints.py
@@ -9,4 +9,16 @@ def get_world_checkpoints():
 
     checkpoints.sort(key=lambda x: x[3], reverse=True)  # Sort by date added
 
+    res = []
+
+    for checkpoint in checkpoints:
+        res.append(
+            {
+                "name": checkpoint[1],
+                "coords": checkpoint[2],
+                "day": checkpoint[3].strftime("%d/%m/%Y"),
+                "hour": checkpoint[3].strftime("%H:%M:%S"),
+            }
+        )
+
     return checkpoints
