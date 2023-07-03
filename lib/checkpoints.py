577f5dbee1e4ec26cd64cac2be36c99e45471311
AimbotParce <marcparcerisa@gmail.com>
2023-07-03 22:52:20 +0200

    sort by date

--- lib/checkpoints.py
+++ lib/checkpoints.py
@@ -7,6 +7,6 @@ get_query = "SELECT id, name, coords, date_added FROM checkpoints;"
 def get_world_checkpoints():
     checkpoints = sql(get_query)
 
-    checkpoints.sort(key=lambda x: x[0], reverse=True)
+    checkpoints.sort(key=lambda x: x[3], reverse=True)  # Sort by date added
 
     return checkpoints
