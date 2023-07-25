24cc450ae532bead24b626c4a5be21c30fbc688c
AimbotParce <marcparcerisa@gmail.com>
2023-07-26 01:29:03 +0200

    wtf

--- refresh.sh
+++ refresh.sh
@@ -2,3 +2,4 @@ cp minecraft-web-page.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable minecraft-web-page.service
 systemctl restart minecraft-web-page.service
+# WHAT THE FUCK
