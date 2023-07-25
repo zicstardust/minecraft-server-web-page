d9e4bbc6ed1acfe2609fdc0598ebb1de58e7716c
AimbotParce <marcparcerisa@gmail.com>
2023-07-26 00:54:45 +0200

    Run refresh as sudo

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,4 @@
-sudo cp minecraft-web-page.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable minecraft-web-page.service
-sudo systemctl restart minecraft-web-page.service
+cp minecraft-web-page.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable minecraft-web-page.service
+systemctl restart minecraft-web-page.service
