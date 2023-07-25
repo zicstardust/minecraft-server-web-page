b4b2b0b71dfec070889c9e9ad342cc0c5e91466a
AimbotParce <marcparcerisa@gmail.com>
2023-07-26 01:24:07 +0200

    test

--- refresh.sh
+++ refresh.sh
@@ -2,3 +2,5 @@ cp minecraft-web-page.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable minecraft-web-page.service
 systemctl restart minecraft-web-page.service
+
+# This is another test that I want to run
