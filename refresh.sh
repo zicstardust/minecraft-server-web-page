261591d27959ccd4a75a7b4df1b802b4d4592bf5
AimbotParce <marcparcerisa@gmail.com>
2023-07-25 22:20:47 +0200

    Do not use sudo

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,4 @@
-sudo cp minecraft-web-page.service /etc/systemd/system/
+cp minecraft-web-page.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable minecraft-web-page.service
 systemctl restart minecraft-web-page.service
