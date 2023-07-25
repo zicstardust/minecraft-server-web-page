733aac37e5fd04a141fe0f68bc1f4e61cdbc18b1
AimbotParce <marcparcerisa@gmail.com>
2023-07-26 01:37:05 +0200

    Do not Giovanni Giorgio

--- refresh.sh
+++ refresh.sh
@@ -2,8 +2,3 @@ cp minecraft-web-page.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable minecraft-web-page.service
 systemctl restart minecraft-web-page.service
-
-
-
-
-# My name is Giovanni Giorgio,
