7eee39b0e906e0e7e391b47352a915893a04eccd
AimbotParce <marcparcerisa@gmail.com>
2023-08-02 01:07:25 +0200

    I was parsing folder name instead of file name

--- lib/backups.py
+++ lib/backups.py
@@ -6,19 +6,19 @@ BACKUP_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."
 
 def parse_name(name):
     "Name is in format %Y-%m-%d_%H-%M-%S.zip"
-    return datetime.strptime(name, "%Y-%m-%d_%H-%M-%S.zip")
+    timestamp = datetime.strptime(name, "%Y-%m-%d_%H-%M-%S.zip")
+    day = timestamp.strftime("%Y-%m-%d")
+    hour = timestamp.strftime("%H:%M:%S")
+    return {"name": name, "day": day, "hour": hour}
 
 
 def get_backups():
     backups = []
-    for file in os.listdir(BACKUP_PATH):
-        if os.path.isdir(os.path.join(BACKUP_PATH, file)):
-            section = {"name": file, "backups": []}
-            for backup in os.listdir(os.path.join(BACKUP_PATH, file)):
+    for folder in os.listdir(BACKUP_PATH):
+        if os.path.isdir(os.path.join(BACKUP_PATH, folder)):
+            section = {"name": folder, "backups": []}
+            for file in os.listdir(os.path.join(BACKUP_PATH, folder)):
                 if file.endswith(".zip"):
-                    timestamp = parse_name(backup)
-                    day = timestamp.strftime("%Y-%m-%d")
-                    hour = timestamp.strftime("%H:%M:%S")
-                    section["backups"].append({"name": backup, "day": day, "hour": hour})
+                    section["backups"].append(parse_name(file))
             backups.append(section)
     return backups
