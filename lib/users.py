790aa90d855877e7390abbb3b91d7764ae2a6441
AimbotParce <marcparcerisa@gmail.com>
2023-08-02 20:30:34 +0200

    Markup

--- lib/users.py
+++ lib/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Markup
 from flask_login import login_user, logout_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from .sql import db
@@ -18,7 +18,7 @@ def handle_login(username, password):
     user = User.query.filter_by(username=username).first()
 
     if not user:
-        flash("User does not exist. <a href='{}'>Sign up</a> instead?".format(url_for("signup")))
+        flash(Markup("User does not exist. <a href='{}'>Sign up</a> instead?".format(url_for("signup"))))
         return redirect(url_for("login"))
     elif not check_password_hash(user.password, password):
         flash("Incorrect password")
