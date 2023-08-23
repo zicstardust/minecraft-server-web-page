4ac29907a8bfeae4c42f564cc713888e935a6634
AimbotParce <marcparcerisa@gmail.com>
2023-08-23 22:54:44 +0200

    PipEnv

---
+++ app.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/minecraft-server-web-page')
+
+activate_this = 
--- secrets/generate_secret_key.py
+++ secrets/generate_secret_key.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-KEY_FILE = os.path.join(os.path.dirname(__file__), "..", "config", "web_secret.key")
+KEY_FILE = os.path.join(os.path.dirname(__file__), "web_secret.key")
 
 if __name__ == "__main__":
     with open(KEY_FILE, "wb") as f:
