e25b838919b157876e0962b75e1c78f10be367ae
AimbotParce <marcparcerisa@gmail.com>
2023-08-30 22:25:05 +0200

    Log error

--- secrets/db_credentials.py
+++ secrets/db_credentials.py
@@ -3,6 +3,11 @@ import json
 
 DB_CREDENTIALS_FILE = os.path.join(os.path.dirname(__file__), "db_credentials.json")
 
+if not os.path.exists(DB_CREDENTIALS_FILE):
+    raise FileNotFoundError(
+        f'Could not find {DB_CREDENTIALS_FILE}, please create it and add the keys "user" and "password" with your db credentials.'
+    )
+
 with open(DB_CREDENTIALS_FILE, "r") as f:
     __credentials = json.load(f)
 
