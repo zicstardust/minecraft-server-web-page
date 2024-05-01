74a1c7b0aee3f5b7afe0d6c0529887b1f8f6b8b3
AimbotParce <marcparcerisa@gmail.com>
2024-05-01 20:09:32 +0200

    Remove user logins

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-# Import flask
-import os
-from secrets.secret_key import KEY_FILE
-
-from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
+from flask import Flask, render_template
 from lib.backups import get_backups
 from lib.playtime import get_player_playtime_hours
 from lib.server_status import get_server_status
-from lib.sql import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS, setup_db
-from lib.users import handle_login, handle_logout, handle_signup, setup_login_manager
 
 # Configure for port 80
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
-setup_db(app)
-setup_login_manager(app)
-
-# Configure for secret key
-with open(KEY_FILE, "rb") as f:
-    app.secret_key = f.read()
 
 
 @app.route("/")
@@ -42,42 +28,6 @@ def playtime():
 def api_playtime():
     players, playtime = get_player_playtime_hours()
     return {"players": players, "playtime": playtime}
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@app.route("/login", methods=["POST"])
-def login_post():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    return handle_login(username, password)
-
-
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-
-@app.route("/signup", methods=["POST"])
-def signup_post():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    password_confirm = request.form.get("password-confirm")
-    return handle_signup(username, password, password_confirm)
-
-
-@app.route("/logout")
-def logout():
-    handle_logout()
-    return redirect(url_for("index"))
-
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
 
 
 if __name__ == "__main__":
--- secrets/secret_key.py
+++
@@ -1,8 +0,0 @@
-import os
-import secrets
-
-KEY_FILE = os.path.join(os.path.dirname(__file__), "web_secret.key")
-
-if __name__ == "__main__":
-    with open(KEY_FILE, "wb") as f:
-        f.write(secrets.token_bytes(128))
