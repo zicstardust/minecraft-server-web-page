fd8074545a62445574e09da80e768198d0d2c356
Zic <git@zicstardust.com>
2024-12-21 12:17:47 -0400

    Server Map, Discord link as environment variable

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     server_status = get_server_status()
-    return render_template("index.html", server_status=server_status, server_uri=(os.environ["SERVER_URI"]))
+    return render_template("index.html",
+                           server_status=server_status,
+                           server_uri=(os.environ["SERVER_URI"]),
+                           server_map_url=(os.environ["SERVER_MAP_URL"]),
+                           discord_link=(os.environ["DISCORD_LINK"])
+                           )
 
 
 @app.route("/backups")
