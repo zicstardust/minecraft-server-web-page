59697ae8126b21f2416c34c9637d815251bb99ac
Zic <git@zicstardust.com>
2024-12-21 12:25:31 -0400

    remove playtime

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask, render_template
-from lib.playtime import get_player_playtime_hours
 from lib.server_status import get_server_status
 
 # Configure for port 80
@@ -22,17 +21,6 @@ def index():
                            server_map_url=(os.environ["SERVER_MAP_URL"]),
                            discord_link=(os.environ["DISCORD_LINK"])
                            )
-
-
-@app.route("/playtime")
-def playtime():
-    return render_template("playtime.html")
-
-
-@app.route("/api/playtime")
-def api_playtime():
-    players, playtime = get_player_playtime_hours()
-    return {"players": players, "playtime": playtime}
 
 
 if __name__ == "__main__":
