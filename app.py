89486477663d32d7e013ed4bfc9fdd41461e15be
Zic <git@zicstardust.com>
2024-12-21 12:19:59 -0400

    remove backup endpoint

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from flask import Flask, render_template, send_from_directory
-from lib.backups import get_backup_folder_file, get_backups
+from flask import Flask, render_template
 from lib.playtime import get_player_playtime_hours
 from lib.server_status import get_server_status
 
@@ -25,12 +24,6 @@ def index():
                            )
 
 
-@app.route("/backups")
-def backups():
-    backups = get_backups()
-    return render_template("backups.html", backups=backups)
-
-
 @app.route("/playtime")
 def playtime():
     return render_template("playtime.html")
@@ -40,15 +33,6 @@ def playtime():
 def api_playtime():
     players, playtime = get_player_playtime_hours()
     return {"players": players, "playtime": playtime}
-
-
-@app.route("/api/backups/download/<path:path>")
-def download_backup(path: str):
-    res = get_backup_folder_file(path)
-    if res is None:
-        return "File not found", 404
-    folder, file = res
-    return send_from_directory(folder, file, as_attachment=True)
 
 
 if __name__ == "__main__":
